3f390df386e8b5fe0420a99d92c18420edaea546
omidblk <omidmokhtari1999@gmail.com>
2023-03-06 16:02:20 +0330

    add sass to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    app: {
+        head: {
+          htmlAttrs: { lang: "en" },
+        },
+      },
+      css: ["@/assets/styles/main.sass"],
+      vite: {
+        css: {
+          preprocessorOptions: {
+            sass: {
+              additionalData: '@import "@/assets/styles/_variables.sass"',
+            },
+          },
+        },
+      },
+  })
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style lang="sass" scoped>
+h1
+    color: $primary
 </style>
