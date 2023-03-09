89844be4d34db5bcdbbf4dbc8cbab61f51c27069
omidblk <omidmokhtari1999@gmail.com>
2023-03-09 18:00:44 +0330

    add banners product to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
         <SliderApp />
-        <section class="up-banners">
-            <BannerApp/>
-            <BannerApp/>
-            <BannerApp/>
-        </section>
-        <section class="between-banners">
-            <BannerApp/>
-            <BannerApp/>
-        </section>
-        <section class="down-banner">
-            <BannerApp/>
-        </section>
+        <div class="container">
+            <section class="up-banners">
+                <BannerApp />
+                <BannerApp />
+                <BannerApp />
+            </section>
+            <section class="between-banners">
+                <BannerApp />
+                <BannerApp />
+            </section>
+            <section class="down-banner mb-5">
+                <BannerApp />
+            </section>
+
+            <!-- last-product -->
+            <BannerProducts />
+
+            <!-- best-product -->
+            <BannerProducts/>
+
+            <!-- best-product -->
+            <BannerProducts/>
+        
+        </div>
+
     </div>
 </template>
 <script setup>
@@ -22,24 +35,26 @@
 
 <style lang="scss">
 @import '../assets/styles/main.scss';
-.up-banners{
-    height:400px;
+
+.up-banners {
+    height: 400px;
     display: grid;
-    grid-template-columns: repeat(3,1fr);
+    grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-    margin: 2rem 10rem;
-}
-.between-banners{
-    height:400px;
-    display: grid;
-    grid-template-columns: repeat(2,1fr);
-    gap: 2rem;
-    margin: 2rem 10rem;
-}
-.down-banner{
-    height:400px;
-    margin: 2rem 10rem;
+    margin: 2rem 0;
 }
 
+.between-banners {
+    height: 400px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+    margin: 2rem 0;
+}
+
+.down-banner {
+    height: 400px;
+    margin: 2rem 0;
+}
 </style>
 
