9f7b40d0d2eca51503dc40523fb825093daab7ee
omidblk <omidmokhtari1999@gmail.com>
2023-03-22 22:37:19 +0430

    add product page and pagination & product card component

---
+++ components/ProductViwe.vue
@@ -0,0 +1,38 @@
+<template>
+    <main class="product-view container d-flex flex-column align-items-center">
+        <BreadCrump class="align-self-start"/>
+        <section class="products-view mt-4">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+        </section>
+        <PaginationApp class="mt-4"/>
+        
+    </main>
+</template>
+<script setup>
+
+</script>
+
+
+<style lang="scss">
+@import "../assets/styles/main.scss";
+.product-view{
+    .products-view{
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    gap: 10px;
+}
+}
+
+    
+</style>
---
+++ components/productCard.vue
@@ -0,0 +1,77 @@
+<template>
+    <article class="card rounded p-4 d-flex flex-column justify-content-between align-items-center">
+        <a href="#" class=" d-flex flex-column justify-content-center align-items-center">
+            <img src="https://placehold.jp/200x200.png" alt="">
+            <h2 class="product-name my-3">رز جاویدان</h2>
+            <div class="price w-100 d-flex justify-content-between">
+                <span class="fs-6 text-decoration-line-through">40000 تومان</span>
+                <span class="offer rounded-pill px-2 d-flex justify-content-center align-items-center">20%</span>
+            </div>
+            <span class="fs-5 mt-2">32000 تومان</span>
+        </a>
+        <button class="rounded m-4 w-100 py-2">+ افزودن به سبد خرید</button>
+        <div class="time-offer w-75">
+            <span class="time-numeric">04 : 15 : 27</span>
+            <div class="time-bar w-100">
+                <div class="time-left rounded"></div>
+            </div>
+        </div>
+
+
+    </article>
+</template>
+
+<script setup>
+</script>
+
+
+<style lang="scss">
+@import "../assets/styles/main.scss";
+
+.card {
+    height: 500px;
+    width: 300px;
+    box-shadow: $box-shadow ;
+
+    .product-name {
+        font-size: 1.5rem;
+    }
+
+    .price {
+        direction: ltr;
+
+        .offer {
+            color: white;
+            background-color: rgb(236, 78, 78);
+        }
+    }
+    button{
+        color: white;
+        background-color: $secondary;
+        transition: all .2s linear;
+        &:hover{
+            color: black;
+            background-color: $third;
+        }
+        
+    }
+    .time-offer{
+        direction: ltr;
+        .time-numeric{
+            color: rgb(236, 78, 78);
+
+        }
+        .time-bar{
+            background-color: bisque;
+            direction: ltr;
+            .time-left{
+                height: 4px;
+                width: 50px;
+                background-color: rgb(236, 78, 78);
+
+            }
+        }
+    }
+
+}
+</style>
